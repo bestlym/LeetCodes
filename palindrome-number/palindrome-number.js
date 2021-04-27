3c85132eb673475cc7dc269002ed55592d1ec271
bestlym <bestlym@gmail.com>
2021-04-27 13:11:03 -0700

    Time: 236 ms (29.41%), Space: 48.8 MB (61.16%) - LeetHub

---
+++ palindrome-number/palindrome-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    const y = x.toString().split('').reverse().join('');
+    if(y===x.toString()){
+        return true;
+    } else {
+        return false;
+    }    
+};
