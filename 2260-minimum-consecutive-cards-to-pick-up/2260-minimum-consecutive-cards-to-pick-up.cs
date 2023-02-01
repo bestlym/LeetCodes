343aa5dfc41bd1595e058a11d86e7c1289044150
bestlym <bestlym@gmail.com>
2023-02-01 15:48:20 -0800

    Time: 259 ms (87.50%), Space: 56.1 MB (83.33%) - LeetHub

---
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int MinimumCardPickup(int[] cards) {
+        int min = int.MaxValue;
+        int ret = 0, left=0;
+        HashSet<int> hs = new HashSet<int>();
+        
+        for(int i=0; i < cards.Length; i++){
+            if(hs.Contains(cards[i])){
+                while(hs.Contains(cards[i]) && hs.Count > 0 ){
+                    hs.Remove(cards[left]);
+                    left++;
+                }
+                min = Math.Min(min, hs.Count+2);
+            }
+            
+            hs.Add(cards[i]);
+        }
+        
+        return min == int.MaxValue ? -1 : min;
+    }
+}
+
+
+
