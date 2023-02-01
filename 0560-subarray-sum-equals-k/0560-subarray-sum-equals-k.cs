6a4d81ef5556aca1a72132e8239342ee20261c52
bestlym <bestlym@gmail.com>
2023-02-01 15:46:32 -0800

    Time: 132 ms (86.84%), Space: 49.8 MB (18.75%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int SubarraySum(int[] nums, int k) {
+        int ret = 0, curr = 0;
+        Dictionary<int, int> dic = new Dictionary<int, int>{[0] = 1};
+        
+        for(int i = 0; i < nums.Length; i++){
+            curr += nums[i];
+            ret += dic.GetValueOrDefault(curr-k);
+            dic[curr] = dic.GetValueOrDefault(curr) + 1;
+        }        
+        return ret;
+    }
+}
