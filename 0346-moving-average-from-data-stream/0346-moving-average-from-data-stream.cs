492f85dd0128f5c14d6f1cd5953598149a7a9c66
bestlym <bestlym@gmail.com>
2023-02-01 15:58:02 -0800

    Time: 124 ms (87.83%), Space: 50.3 MB (24.35%) - LeetHub

---
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.cs
@@ -0,0 +1,27 @@
+public class MovingAverage {
+    
+    private Queue<int> queue;
+    private int Size;
+    private double sum;
+
+    public MovingAverage(int size) {
+        queue = new Queue<int>();
+        Size = size;        
+    }
+    
+    public double Next(int val) {
+        sum += val;
+        queue.Enqueue(val);
+        
+        if(queue.Count > Size)
+            sum -= queue.Dequeue();
+        
+        return sum / queue.Count;
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.Next(val);
+ */
