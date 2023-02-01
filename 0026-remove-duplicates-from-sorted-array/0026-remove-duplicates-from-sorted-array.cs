15253d9ed817ee43fe6f215817ffea844e762e80
bestlym <bestlym@gmail.com>
2023-02-01 15:54:09 -0800

    Time: 137 ms (97.42%), Space: 46.1 MB (47.98%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        if(nums.Length == 0 || nums == null) return 0;
+        
+        int j = 0;
+        
+        for(int i=0; i < nums.Length; i++){
+            if(i < 1 || nums[i] > nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
