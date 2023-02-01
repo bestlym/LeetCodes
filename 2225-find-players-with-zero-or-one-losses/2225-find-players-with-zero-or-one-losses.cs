47566f8b69490c3576f0338c21ef1f42f9312b27
bestlym <bestlym@gmail.com>
2023-02-01 15:48:30 -0800

    Time: 1165 ms (14.22%), Space: 69.8 MB (64.68%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public IList<IList<int>> FindWinners(int[][] matches) {
+        List<IList<int>> ret = new List<IList<int>>();
+        if(matches == null || matches.Length == 0)
+            return ret;
+        SortedDictionary<int, int> dic = new SortedDictionary<int, int>();
+        
+        foreach(var match in matches){
+            dic[match[0]] = dic.GetValueOrDefault(match[0], 0);
+            dic[match[1]] = dic.GetValueOrDefault(match[1], 0) + 1;
+        }
+        
+        List<int> retList1 = new List<int>();
+        List<int> retList2 = new List<int>();
+        
+        foreach(var d in dic){
+            if(d.Value == 0) retList1.Add(d.Key);
+            else if(d.Value == 1) retList2.Add(d.Key);
+        }
+        ret.Add(retList1);
+        ret.Add(retList2);
+            
+        return ret;
+    }    
+}
