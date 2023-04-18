52d667137730483d96014f4b23d426bee8649ff8
bestlym <bestlym@gmail.com>
2023-04-17 19:45:30 -0700

    Time: 70 ms (96.56%), Space: 38.5 MB (67.81%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -1,18 +1,7 @@
-/**
- * @param {string} str
- * @returns {string}
- */
-var reverseWords = function(str) {
-    var result = [];
-    if(str===null||str.length===0) return ''; // when the string is empty
-    var words = str.match(/\S+/g);
-    if(words===null) return ''; //if the str only contain whitespaces
-    var len = words.length;
-    
-    for(var i = 0; i < len; i++){
-        var last = words.pop();
-        words.splice(i, 0, last);
+public class Solution {
+    public string ReverseWords(string s) {
+        var res = s.Trim().Split(" ").Where(x => !x.Trim().Equals(string.Empty)).Reverse();
+        return string.Join(" ", res);
+        
     }
-    
-    return words.join(' ');
-};
+}
