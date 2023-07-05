eb5d0b08def4583212c7712cfe8d048e785ca5ba
bestlym <bestlym@gmail.com>
2023-07-05 16:13:43 -0700

    Time: 156 ms (7.64%), Space: 43.4 MB (52.18%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
@@ -1,7 +1,8 @@
 public class Solution {
     public int[] RunningSum(int[] nums) {
         
-        for(var i=1; i < nums.Length; i++){
+        for(var i = 1; i < nums.Length; i++)
+        {
             nums[i] = nums[i-1] + nums[i];
         }
         
