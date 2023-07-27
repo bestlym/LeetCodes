efe47f22f7042c6dda8e880929c54c19be08bff5
bestlym <bestlym@gmail.com>
2023-07-27 04:30:31 -0700

    Time: 80 ms (67.37%), Space: 41.6 MB (96.59%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cs
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cs
@@ -1,19 +1,19 @@
 public class Solution {
     public int MaxVowels(string s, int k) {
         
-        var arr = new HashSet<char>{'a', 'e', 'i', 'o', 'u'};
+        char[] arr = {'a', 'e', 'i', 'o', 'u'};
         
         int maxCnt = 0;
         int vowCnt = 0;
         
         for(int i = 0; i < k; i++)
         {
-            if(arr.Contains(s[i])) vowCnt++;
+            if(Array.IndexOf(arr, s[i]) >=0 ) vowCnt++;
         }
         maxCnt = vowCnt;
         for(int i = k; i < s.Length; i++){
-            if(arr.Contains(s[i-k])) vowCnt--;
-            if(arr.Contains(s[i])) vowCnt++;
+            if(Array.IndexOf(arr, s[i-k]) >= 0) vowCnt--;
+            if(Array.IndexOf(arr, s[i]) >= 0) vowCnt++;
             maxCnt = Math.Max(maxCnt, vowCnt);
         }
         
