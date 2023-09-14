ea142d0e583da3b6f1f355d457d0a035036348bf
bestlym <bestlym@gmail.com>
2023-09-13 18:55:02 -0700

    Time: 158 ms (50.97%), Space: 60.8 MB (14.63%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cs
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cs
@@ -1,18 +1,23 @@
 public class Solution {
     public int[] SortedSquares(int[] nums) {
-        int left = 0, right = nums.Length-1;
-        int[] ret = new int[nums.Length];
+        int numLen = nums.Length;
         
-        for(int i = nums.Length-1; i >= 0; i--){
-            if(Math.Abs(nums[right]) > Math.Abs(nums[left]))
+        int left = 0;
+        int right = numLen - 1;
+        
+        int[] ret = new int[numLen];
+        
+        for(int i = numLen-1; i >= 0; i--){
+            if(Math.Abs(nums[left]) > Math.Abs(nums[right]))
             {
-                ret[i] = nums[right] * nums[right];
-                right--;
-            } else {
                 ret[i] = nums[left] * nums[left];
                 left++;
+            } else {
+                ret[i] = nums[right] * nums[right];
+                right--;
             }
         }
+        
         return ret;
     }
 }
