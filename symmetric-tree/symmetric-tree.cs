c65b660d288ed00349332e0fc82c0ce6ed539f12
bestlym <bestlym@gmail.com>
2023-10-12 13:27:50 -0700

    Time: 89 ms, Memory: 40.7 MB - LeetHub

---
+++ symmetric-tree/symmetric-tree.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSymmetric(TreeNode root) {
+        
+        if(root == null) return true;
+        return DFS(root.left, root.right);
+    }
+    
+    public bool DFS(TreeNode left, TreeNode right)
+    {
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        
+        if(left.val == right.val)
+            return DFS(left.left, right.right) && DFS(left.right, right.left);
+        
+        return false;        
+    }
+}
