5ce76b4021b98b3d2204f1cb928f7564e2486034
bestlym <bestlym@gmail.com>
2023-10-12 00:20:05 -0700

    Time: 133 ms, Memory: 43.2 MB - LeetHub

---
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] Intersect(int[] nums1, int[] nums2) {
+        int first=0, second=0;
+        List<int> ret = new List<int>();
+        
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+        
+        while(nums1.Length > first && nums2.Length > second)
+        {
+            if(nums1[first] == nums2[second])
+            {
+                ret.Add(nums1[first]);
+                first++;
+                second++;                
+            } else if(nums2[second] > nums1[first])
+            {
+                first++;
+            } else {
+                second++;
+            }
+        }
+        
+        return ret.ToArray();
+    }
+}
