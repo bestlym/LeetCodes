c836a33d007128a8e3da9b946142727d3ad312f1
bestlym <bestlym@gmail.com>
2023-10-23 10:35:13 -0700

    Time: 97 ms, Memory: 39.9 MB - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        int sLen = 200;
+        StringBuilder sb = new StringBuilder();
+        
+        foreach(var str in strs)
+        {
+            sLen = Math.Min(sLen, str.Length);
+        }
+        
+        if(sLen == 0) return "";
+        
+        for(int i=0; i < sLen; i++)
+        {
+            char cs = strs[0][i];
+            for(int j = 0; j < strs.Length; j++) {
+                if(cs != strs[j][i]){
+                    return sb.ToString();
+                }
+            }
+            sb.Append(cs);
+        }
+        return sb.ToString();
+    }
+}
+
