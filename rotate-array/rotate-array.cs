50204e0bd8046849634dd6c0d56415f71ec52ea2
bestlym <bestlym@gmail.com>
2024-02-13 13:23:53 -0800

    Time: 148 ms, Memory: 65 MB - LeetHub

--- rotate-array/rotate-array.cs
+++ rotate-array/rotate-array.cs
@@ -1,14 +1,12 @@
 public class Solution {
     public void Rotate(int[] nums, int k) {
-        if(nums == null || nums.Length < 2)
-            return;
+        if(nums.Length < 2 || nums == null) return;
         
         k %= nums.Length;
         
         Reverse(nums, 0, nums.Length-1);
         Reverse(nums, 0, k-1);
         Reverse(nums, k, nums.Length-1);
-        
     }
     
     public void Reverse(int[] nums, int left, int right)
@@ -20,6 +18,6 @@ public class Solution {
             nums[right] = temp;
             left++;
             right--;
-        }
+        }        
     }
 }
