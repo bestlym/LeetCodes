dbf81c47201aa77173e7b57d91924cd5e0f2c381
bestlym <bestlym@gmail.com>
2024-02-13 13:27:37 -0800

    Time: 269 ms, Memory: 68.1 MB - LeetHub

--- contains-duplicate/contains-duplicate.cs
+++ contains-duplicate/contains-duplicate.cs
@@ -1,11 +1,14 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
+        if(nums.Length < 2 || nums == null) return false;
+        
         HashSet<int> hs = new HashSet<int>();
         
         foreach(int n in nums)
         {
             if(!hs.Add(n)) return true;
         }
+        
         return false;
     }
 }
