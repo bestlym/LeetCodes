e92c46f7b5a10cc7e170e8e06033b6df021bad61
bestlym <bestlym@gmail.com>
2024-02-13 13:16:16 -0800

    Time: 73 ms, Memory: 42.9 MB - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
@@ -3,17 +3,17 @@ public class Solution {
     
     public int MaxProfit(int[] prices) {
        
-        if(prices == null || prices.Length == 0) return 0;
+        if(prices.Length == 0 || prices == null) return 0;
         
         for(int i=1; i < prices.Length; i++)
         {
-            if(prices[i] - prices[i-1] > 0){
+            if(prices[i] - prices[i-1] > 0)
+            {
                 prices[i-1] = prices[i] - prices[i-1];
             } else {
                 prices[i-1] = 0;
             }
         }
-        
         prices[prices.Length-1] = 0;
         return prices.Sum();
     }
