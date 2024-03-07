90c5219a76d8c4ec92114e022621d27c739885b7
bestlym <bestlym@gmail.com>
2024-03-06 23:25:57 -0800

    Time: 41 ms, Memory: 37.6 MB - LeetHub

--- implement-strstr/implement-strstr.cs
+++ implement-strstr/implement-strstr.cs
@@ -2,16 +2,17 @@ public class Solution {
     public int StrStr(string haystack, string needle) {
         if(needle.Length > haystack.Length) return -1;
         
-        for(int i =0; i <= haystack.Length - needle.Length; i++)
+        for(int i=0; i <= haystack.Length-needle.Length; i++)
         {
-            int j = 0;
-            for(;j < needle.Length; j++)
+            int j=0;
+            for(; j < needle.Length; j++)
             {
                 if(haystack[j+i] == needle[j]) continue;
                 break;
             }
-            if(j == needle.Length) return i;
+            if(j == needle.Length) return i;                
         }
+        
         return -1;
     }
 }
