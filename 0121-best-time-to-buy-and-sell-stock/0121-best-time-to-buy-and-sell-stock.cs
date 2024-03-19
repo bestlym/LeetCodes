b108cb42e29c5019e2985b1a47c14a1aabcc1858
bestlym <bestlym@gmail.com>
2024-03-19 13:07:00 -0700

    Time: 333 ms (85.79%), Space: 58.3 MB (35.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -2,8 +2,9 @@ public class Solution {
     public int MaxProfit(int[] prices) {
         int buyPrice = prices[0];
         int profit = 0;
-            
-        foreach(int p in prices){
+        
+        foreach(int p in prices)
+        {
             buyPrice = Math.Min(buyPrice, p);
             profit = Math.Max(profit, p-buyPrice);
         }
