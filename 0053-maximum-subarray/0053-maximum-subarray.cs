e61081a7c3be420116d5f9cf24a817d8e8282846
bestlym <bestlym@gmail.com>
2024-03-19 13:42:14 -0700

    Time: 288 ms (38.08%), Space: 62.6 MB (62.12%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cs
+++ 0053-maximum-subarray/0053-maximum-subarray.cs
@@ -3,14 +3,16 @@ public class Solution {
         if(nums.Length == 0) return 0;
         if(nums.Length == 1) return nums[0];
         
-        int ret = int.MinValue;
-        int sum = 0;
+        int sum=0, ret=int.MinValue;
         
-        for(int i=0; i < nums.Length; i++){
-            sum += nums[i];
+         foreach(int n in nums)
+         {
+            sum += n;
             ret = Math.Max(sum, ret);
             if(sum < 0) sum = 0;
         }
+
+        
         return ret;
     }
 }
