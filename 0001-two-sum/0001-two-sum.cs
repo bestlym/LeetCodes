94dcfc599d600a4840b86d1c3912fca17386de40
bestlym <bestlym@gmail.com>
2024-03-19 12:49:25 -0700

    Time: 115 ms (78.48%), Space: 48.3 MB (14.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,19 +1,19 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        if(nums == null || nums.Length < 2) return new int[2];
+        if(nums.Length < 2 || nums == null) return new int[2];
         
         Dictionary<int, int> dic = new Dictionary<int, int>();
         
-        for(var i = 0; i < nums.Length; i++)
+        for(int i=0; i < nums.Length; i++)
         {
-            if(dic.TryGetValue(target - nums[i], out int r))
+            if(dic.TryGetValue(target-nums[i], out int r))
             {
-                return new int[2]{r, i};
-            }
-            dic[nums[i]] = i;
+                return new int[2]{i, r};
+            } 
+            
+            dic[nums[i]] = i;            
         }
         
-        return new int[2];  
-
+        return new int[2];
     }
 }
