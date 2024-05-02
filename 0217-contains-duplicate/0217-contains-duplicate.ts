ebb60f9ed7931d681a8e47254fdba2fe87af7c44
bestlym <bestlym@gmail.com>
2024-05-01 23:25:50 -0700

    Time: 82 ms (61.92%), Space: 61.7 MB (76.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -1,8 +1,4 @@
 function containsDuplicate(nums: number[]): boolean {
-    const numsMap = new Map();
-    for(let i=0; i < nums.length; i++){
-        if(numsMap.has(nums[i])) return true;
-        numsMap.set(nums[i], i);
-    }
-    return false;
+    const set = new Set<number>(nums);
+    return set.size < nums.length;
 };
