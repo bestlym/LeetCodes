3257a400d29b305d57371cfe8625bf22d743aa4c
bestlym <bestlym@gmail.com>
2024-05-04 18:14:38 -0700

    Time: 150 ms, Memory: 63.2 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.cs
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cs
@@ -1,27 +1,21 @@
 public class Solution {
     public int[] SortedSquares(int[] nums) {
-        int numsLength = nums.Length;
         
-        int left = 0;
-        int right = numsLength - 1;
+        var ret = new int[nums.Length];
+        int left = 0; 
+        int right = nums.Length-1;
         
-        
-        int[] ret = new int[numsLength];
-        
-        for(int i = right; i >= 0; i--)
+        for(int i=nums.Length-1; i >= 0; i--)
         {
             if(Math.Abs(nums[left]) > Math.Abs(nums[right]))
             {
                 ret[i] = nums[left] * nums[left];
                 left++;
-                
             } else {
                 ret[i] = nums[right] * nums[right];
                 right--;
             }
-        }
-        
-        return ret;
-        
+        }        
+        return ret;        
     }
 }
