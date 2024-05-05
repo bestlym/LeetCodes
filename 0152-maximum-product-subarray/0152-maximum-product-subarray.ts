488243d9eb3675ed58b5d41bf7c41b97ca23d909
bestlym <bestlym@gmail.com>
2024-05-05 02:39:21 -0700

    Time: 62 ms (61.56%), Space: 53.2 MB (17.72%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.ts
@@ -0,0 +1,15 @@
+function maxProduct(nums: number[]): number {
+    let ret: number = nums[0];
+    let min: number=1;
+    let max: number=1;
+    
+    nums.forEach((n) => {
+        const preVal = [min*n, max*n, n];
+        max = Math.max(...preVal);
+        min = Math.min(...preVal);
+        
+        ret = Math.max(max, ret);
+    });
+    
+    return ret;
+};
