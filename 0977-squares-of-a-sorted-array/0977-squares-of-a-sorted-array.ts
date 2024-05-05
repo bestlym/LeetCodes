11e8a4b1d9a1dad8092fffc08b9c04263e112933
bestlym <bestlym@gmail.com>
2024-05-04 18:33:04 -0700

    Time: 104 ms (17.31%), Space: 57.8 MB (11.22%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.ts
@@ -0,0 +1,17 @@
+function sortedSquares(nums: number[]): number[] {
+    const ret: number[] = [];
+    let left: number = 0;
+    let right: number = nums.length-1;
+    
+    for(let i = nums.length-1; i >= 0; i--){
+        if(Math.abs(nums[right]) > Math.abs(nums[left])){
+            ret[i] = nums[right] * nums[right];
+            right--;
+        } else {
+            ret[i] = nums[left] * nums[left];
+            left++;
+        }
+    }
+    return ret;
+    //return nums.map((num) => num ** 2).sort((a, b) => a - b); //one line approach
+};
