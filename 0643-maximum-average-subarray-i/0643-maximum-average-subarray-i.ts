2aaeabcd3b9eef721643f9c1bb7df68ed42533ed
bestlym <bestlym@gmail.com>
2024-05-04 23:16:48 -0700

    Time: 87 ms (53.01%), Space: 63.6 MB (86.35%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
@@ -1,19 +1,14 @@
 function findMaxAverage(nums: number[], k: number): number {
     let sum: number=0;
-    let ret: number=0;
-    let left: number=0;
     
     for(let i=0; i < k; i++){
-        sum += nums[i];
-        
+        sum += nums[i];        
     }
 
-    ret = sum;
+    let ret: number = sum;
     for(let i=k; i < nums.length;i++){
-        sum += (nums[i]-nums[left]);
-        ret = Math.max(sum, ret);
-        left++;        
-        
+        sum += (nums[i]-nums[i-k]);
+        ret = Math.max(sum, ret);        
     }
     
     return ret / k;
