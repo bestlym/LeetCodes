465968cd184ce05d6840de4dc05c1bb3e39a2700
bestlym <bestlym@gmail.com>
2024-05-05 03:23:09 -0700

    Time: 134 ms (90.36%), Space: 53.9 MB (19.28%) - LeetHub

--- 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.cs
+++ 0487-max-consecutive-ones-ii/0487-max-consecutive-ones-ii.cs
@@ -1,20 +1,20 @@
 public class Solution {
     public int FindMaxConsecutiveOnes(int[] nums) {
-        int right = 0, left = 0, zeroCnt = 0, ret=0;
-        while(right < nums.Length)
-        {
+        int right=0, left=0, ret=0, zeroCnt=0;
+        
+        while(right < nums.Length){
             if(nums[right] == 0){
                 zeroCnt++;
+                
                 while(zeroCnt > 1){
-                    if(nums[left] == 0)
-                        zeroCnt--;
+                    if(nums[left] == 0){
+                        zeroCnt--;                        
+                    }
                     left++;
                 }
-                    
             }
-            
-            ret = Math.Max(ret, right-left+1);
             right++;
+            ret = Math.Max(ret, right-left);
         }
         return ret;
     }
