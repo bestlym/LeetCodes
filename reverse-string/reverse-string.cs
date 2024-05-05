29fd5ebbf8130ad98d367bafc433657141dbc757
bestlym <bestlym@gmail.com>
2024-05-04 17:56:50 -0700

    Time: 241 ms, Memory: 129.1 MB - LeetHub

--- reverse-string/reverse-string.cs
+++ reverse-string/reverse-string.cs
@@ -1,18 +1,16 @@
 public class Solution {
     public void ReverseString(char[] s) {
-        var left = 0;
-        var right = s.Length-1;
+        int left = 0;
+        int right = s.Length-1;
         
-        while(right > left)
+        while(left < right)
         {
-            var tempLeft = s[left];
-            var tempRight = s[right];
-            
-            s[left] = tempRight;
-            s[right] = tempLeft;
+            var temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
             
             left++;
-            right--;
-        }                
+            right--;            
+        }
     }
 }
