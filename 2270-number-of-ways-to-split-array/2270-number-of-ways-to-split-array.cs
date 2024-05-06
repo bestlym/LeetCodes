9b5325d8be39ddfeb7844e1290adf6372156a725
bestlym <bestlym@gmail.com>
2024-05-05 20:52:38 -0700

    Time: 362 ms (77.27%), Space: 63.2 MB (86.36%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cs
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.cs
@@ -1,14 +1,12 @@
 public class Solution {
     public int WaysToSplitArray(int[] nums) {
-        var pre = new int[nums.Length];
-        long sum=0, currSum=0;
+        var ret = 0;
+        long sum=0, currSum=0;        
         
         foreach(var n in nums)
         {
             sum += n;
-        }
-        
-        var ret = 0;
+        }        
         
         for(int i=0; i < nums.Length-1; i++)
         {
