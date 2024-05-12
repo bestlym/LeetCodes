9eb01819f4a39871a1f1f6b831d3fb5639f201f1
bestlym <bestlym@gmail.com>
2024-05-11 20:18:44 -0700

    Time: 106 ms (94.11%), Space: 48.4 MB (9.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -4,14 +4,13 @@ public class Solution {
         
         Dictionary<int, int> dic = new Dictionary<int, int>();
         
-        for(int i=0; i < nums.Length; i++)
+        for(int i=0; i<nums.Length; i++)
         {
-            if(dic.TryGetValue(target-nums[i], out int r))
-            {
+            if(dic.TryGetValue(target - nums[i], out int r)){
                 return new int[2]{i, r};
-            } 
-            
-            dic[nums[i]] = i;            
+            }
+            //dic.Add(nums[i], i);
+            dic[nums[i]] = i;     
         }
         
         return new int[2];
