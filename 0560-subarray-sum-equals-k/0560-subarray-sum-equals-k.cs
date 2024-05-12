8880c49b1679be739aad41e8c534f54ba8cac249
bestlym <bestlym@gmail.com>
2024-05-12 04:24:28 -0700

    Time: 150 ms (36.60%), Space: 51.4 MB (34.18%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
@@ -3,8 +3,8 @@ public class Solution {
         int ret = 0, curr = 0;
         Dictionary<int, int> dic = new Dictionary<int, int>{[0] = 1};
         
-        for(int i = 0; i < nums.Length; i++){
-            curr += nums[i];
+        foreach(int n in nums){
+            curr += n;
             ret += dic.GetValueOrDefault(curr-k, 0);
             dic[curr] = dic.GetValueOrDefault(curr, 0) + 1;
         }        
