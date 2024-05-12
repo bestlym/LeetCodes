02b59b6d625c610563993a99d719faa45da242e1
bestlym <bestlym@gmail.com>
2024-05-11 20:47:40 -0700

    Time: 49 ms (85.26%), Space: 39.4 MB (65.32%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cs
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cs
@@ -2,7 +2,7 @@ public class Solution {
     public bool CheckIfPangram(string sentence) {
         if(sentence.Length < 26) return false;
         
-        var hs = new HashSet<char>(26);
+        var hs = new HashSet<char>();
         
         foreach(char c in sentence)
         {
@@ -10,6 +10,5 @@ public class Solution {
         }
         
         return hs.Count == 26;
-        
     }
 }
