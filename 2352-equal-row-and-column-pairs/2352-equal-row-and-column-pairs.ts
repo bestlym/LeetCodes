246bb56ad536f17855209c60f5034a90fe2a2868
bestlym <bestlym@gmail.com>
2024-05-13 07:20:37 -0700

    Time: 118 ms (42.64%), Space: 61.1 MB (36.64%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.ts
@@ -0,0 +1,20 @@
+function equalPairs(grid: number[][]): number {
+    const map: Map<string, number> = new Map();
+    let ret: number = 0;
+    
+    grid.forEach((item)=>{
+        const arrStr:string = item.join(' ');
+        map[arrStr] = (map[arrStr] || 0) +1;
+    });
+    console.log(map);
+    for(let i=0; i< grid.length; i++){
+        const arr = [];
+        for(let j=0; j < grid.length; j++){
+            arr.push(grid[j][i]);
+        }
+
+        ret+= (map[arr.join(' ')] || 0);
+    }
+    
+    return ret;
+};
