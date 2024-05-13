1f4557319a97357939caa465d28f728ca455177c
bestlym <bestlym@gmail.com>
2024-05-13 01:20:20 -0700

    Time: 111 ms (78.67%), Space: 64.8 MB (18.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.ts
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -2,10 +2,10 @@ function groupAnagrams(strs: string[]): string[][] {
     const hm: Map<string, string[]> = new Map();
     const ret: string[][] = [];
     for(const s of strs){
-        const charArray = [...s];
-        charArray.sort();
-        let keyStr = charArray.toString();
-        console.log(keyStr);
+        //const charArray = [...s];
+        //charArray.sort();
+        //let keyStr = charArray.toString();
+        const keyStr = s.split('').sort().join('');
         
         if(!hm.has(keyStr)) hm.set(keyStr, []);
         hm.get(keyStr).push(s);
