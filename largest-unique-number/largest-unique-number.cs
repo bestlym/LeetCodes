9f930c712850a287823465812adb4181ab3dcfa7
bestlym <bestlym@gmail.com>
2024-05-12 19:21:32 -0700

    Time: 70 ms, Memory: 41.1 MB - LeetHub

--- largest-unique-number/largest-unique-number.cs
+++ largest-unique-number/largest-unique-number.cs
@@ -1,22 +1,11 @@
 public class Solution {
-    public int LargestUniqueNumber(int[] nums) {
-        int ret = -1;        
-        if(nums.Length == 0 || nums == null) return ret;
-        
-        SortedDictionary<int, int> dic = new SortedDictionary<int, int>();
+    public int LargestUniqueNumber(int[] nums) {        
+        Dictionary<int, int> dic = new  Dictionary<int, int>();
         
         foreach(var n in nums){
             dic[n] = dic.GetValueOrDefault(n, 0) + 1;            
-        }
-        
-        foreach(var d in dic){
-            if(d.Value == 1){
-                ret = Math.Max(ret, d.Key);
-            }
-        }
-        
-        int ret2 = dic.Where(w=>w.Value == 1).OrderBy(o=>o.Key).Select(s=>s.Key).LastOrDefault(-1);
-        //return ret;
-        return ret2;
+        }       
+
+        return dic.Where(w=>w.Value == 1).OrderBy(o=>o.Key).Select(s=>s.Key).LastOrDefault(-1);
     }
 }
