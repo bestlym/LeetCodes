81fd38cb78400d1e40348cd4f106095ffead2f16
bestlym <bestlym@gmail.com>
2024-05-13 10:27:13 -0700

    Time: 56 ms (50.24%), Space: 39.8 MB (24.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cs
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -1,25 +1,21 @@
 public class Solution {
     public bool IsValid(string str) {
-        Dictionary<char, char> dic = new Dictionary<char, char>();
+        var dic = new Dictionary<char, char>();
         dic.Add(')', '(');
-        dic.Add(']', '[');
         dic.Add('}', '{');
+        dic.Add(']', '[');
         
-        Stack<char> stack = new Stack<char>();
+        var stack = new Stack<char>();        
         
-        foreach(char c in str){
-            if(dic.ContainsKey(c)){
-                if(stack.Count > 0 && stack.Peek() == dic[c])
-                {
-                    stack.Pop();
-                } else {
-                    return false;
-                }
+        foreach(char c in str)
+        {            
+            if(stack.Count > 0 && dic.ContainsKey(c)){
+                if(stack.Pop() != dic[c]) return false;
             } else {
                 stack.Push(c);
             }
         }
         
-        return stack.Count == 0 ? true: false;
+        return stack.Count == 0;
     }     
 }
