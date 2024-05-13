0efaf53241d2c252421c0a48b41817ecfcb87536
bestlym <bestlym@gmail.com>
2024-05-13 03:23:20 -0700

    Time: 189 ms (37.21%), Space: 56.5 MB (81.40%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cs
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cs
@@ -1,23 +1,19 @@
 public class Solution {
     public int HardestWorker(int n, int[][] logs) {
-        var dic = new Dictionary<int, int>();
         int maxVal = logs[0][1]-0;
         int ret = logs[0][0];
-        //dic[logs[0][1]-0] = logs[0][0];
         
         for(int i=1; i < logs.Length; i++)
         {
             int currCal = logs[i][1]-logs[i-1][1];
             int currId = logs[i][0];
-            //if(maxVal >= currCal) continue;
-            //dic[currCal] = logs[i][0];
+
             if(maxVal < currCal || (maxVal == currCal && currId < ret))
                 ret = currId;
 
             maxVal = Math.Max(maxVal, currCal);            
         }
         
-        //return dic[maxVal];
         return ret;
     }
 }
