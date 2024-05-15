8fc4567600d4fe69f3545e26e5d92ee409ea0f60
bestlym <bestlym@gmail.com>
2024-05-15 09:17:20 -0700

    Time: 202 ms (21.74%), Space: 64.3 MB (64.88%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private int ret = 0;
+    
+    public int GoodNodes(TreeNode root) {
+        DFS(root, int.MinValue);
+        return ret;
+    }
+    
+    public void DFS(TreeNode node, int maxVal)
+    {
+        if(node.val >= maxVal) ret++;
+        
+        if(node.right != null) {
+            DFS(node.right, Math.Max(node.val, maxVal));
+        }
+        
+        if(node.left != null){
+            DFS(node.left, Math.Max(node.val, maxVal));
+        }
+    }
+}
