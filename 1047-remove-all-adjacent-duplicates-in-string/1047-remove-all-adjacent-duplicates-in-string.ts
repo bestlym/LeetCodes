d55ccc59c04d0bed88825d2ff4bb1966e6a0fadd
bestlym <bestlym@gmail.com>
2024-05-14 17:31:26 -0700

    Time: 78 ms (36.99%), Space: 58.3 MB (65.75%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.ts
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.ts
@@ -1,8 +1,10 @@
 function removeDuplicates(s: string): string {
     const stack: string[] = [];
+    //Array.constructor.prototype.peek: any = () => {stack[stack.length-1] || ""};
     
     for(const c of s)
     {
+        //stack.constructor.prototype.peek = () => stack[stack.length-1] || "";
         const peek = () => stack[stack.length-1] || "";
         
         if(peek() == c){
