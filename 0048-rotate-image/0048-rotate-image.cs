16d0d845aeeef8639f98d1eccd9ed6de4db564fd
bestlym <bestlym@gmail.com>
2024-05-16 13:41:44 -0700

    Time: 98 ms (73.50%), Space: 45.8 MB (38.68%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cs
+++ 0048-rotate-image/0048-rotate-image.cs
@@ -1,19 +1,17 @@
 public class Solution {
     public void Rotate(int[][] matrix) {
-        int n = matrix.Length;  //4
-        int l = n-1;
+        int n = matrix.Length;  //3
+        int l = n-1; //2
         
         for(int i=0; i < (n+1) / 2; i++){   /// 2
             for(int j=0; j < n/2; j++){   /// 2
-                int temp = matrix[l-j][i];   //2 1
-                
-                //2 1           1    
-                matrix[l-j][i] = matrix[l-i][l-j];   
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[l-j][i];
+                matrix[l-j][i] = matrix[l-i][l-j];
                 matrix[l-i][l-j] = matrix[j][l-i];
-                matrix[j][l-i] = matrix[i][j];
-                matrix[i][j] = temp;
+                matrix[j][l-i] = temp;
             }
-        }        
+        }
     }
 }
 
