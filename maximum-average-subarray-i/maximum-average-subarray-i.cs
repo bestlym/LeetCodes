0236a33911e54b01dfa5a537a09fe42d51b4e4ac
bestlym <bestlym@gmail.com>
2024-05-16 14:00:20 -0700

    Time: 435 ms, Memory: 59.4 MB - LeetHub

--- maximum-average-subarray-i/maximum-average-subarray-i.cs
+++ maximum-average-subarray-i/maximum-average-subarray-i.cs
@@ -1,9 +1,8 @@
 public class Solution {
     public double FindMaxAverage(int[] nums, int k) {
-        
-        if(nums.Length == 0 || nums == null) return 0.00;
-        
-        int left = 0, right=0, currSum = 0, res = Int32.MinValue;
+        int currSum = 0;
+        int left = 0, right = 0;
+        int ret = int.MinValue;
         
         while(right < nums.Length)
         {
@@ -17,9 +16,9 @@ public class Solution {
             }
             
             if(right-left == k)
-                res = Math.Max(res, currSum);
+                ret = Math.Max(ret, currSum);
         }
         
-        return (double)res / k;
+        return (double)ret/k;
     }
 }
