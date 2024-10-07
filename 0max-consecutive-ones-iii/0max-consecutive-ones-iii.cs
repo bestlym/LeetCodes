7bbe8ccc9c90d3315e92a44605f026e88602bf2d
bestlym <bestlym@gmail.com>
2024-10-07 13:55:51 -0700

    Time: 195 ms, Memory: 61.3 MB - LeetHub

--- 0max-consecutive-ones-iii/0max-consecutive-ones-iii.cs
+++ 0max-consecutive-ones-iii/0max-consecutive-ones-iii.cs
@@ -1,18 +1,23 @@
 public class Solution {
     public int LongestOnes(int[] nums, int k) {
-        int maxLength = 0, left = 0, zeroCnt = 0;
-        for(int right=0; right < nums.Length; right++)
+        int zeroCnt = 0;
+        int ret = 0;
+        int left = 0;
+        
+        for(int i = 0; i < nums.Length; i++)
         {
-            if(nums[right] == 0) zeroCnt++;
+            if(nums[i] == 0) zeroCnt++;
             
             while(zeroCnt > k)
             {
                 if(nums[left] == 0) zeroCnt--;
                 left++;
             }
-            maxLength = Math.Max(maxLength, right-left+1);
+            
+            ret = Math.Max(ret, i-left+1);
         }
         
-        return maxLength;
+        return ret;
+        
     }
 }
