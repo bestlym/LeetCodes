e0514f5b2998f1cc0aac57f37beadd3850d7d27f
bestlym <bestlym@gmail.com>
2024-10-07 20:18:51 -0700

    Time: 61 ms, Memory: 40 MB - LeetHub

--- minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cs
+++ minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.cs
@@ -1,14 +1,14 @@
 public class Solution {
-    public int MinStartValue(int[] nums) {
-
-        var minVal = 0;
-        var sum = 0;
+    public int MinStartValue(int[] nums) {        
+        int minVal = 0;
+        int sum = 0;
         
-        for(int i=0; i < nums.Length; i++){
-            sum += nums[i];
+        foreach(int n in nums)
+        {
+            sum += n;
             minVal = Math.Min(minVal, sum);
         }
-        return 1-minVal;
         
+        return 1-minVal;
     }
 }
