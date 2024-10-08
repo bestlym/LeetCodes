da2e07c38b9bcb6eba59db804887d3bb2d3b2efc
bestlym <bestlym@gmail.com>
2024-10-07 20:10:28 -0700

    Time: 94 ms (88.27%) | Memory: 46.1 MB (64.68%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cs
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cs
@@ -1,13 +1,11 @@
 public class Solution {
     public int[] RunningSum(int[] nums) {
-        int[] ret = new int[nums.Length];
-        ret[0] = nums[0];
-
-        for(int i=1; i < nums.Length; i++)
+        
+        for(var i = 1; i < nums.Length; i++)
         {
-            ret[i] = ret[i-1] + nums[i];
+            nums[i] = nums[i-1] + nums[i];
         }
-
-        return ret;
+        
+        return nums;
     }
 }
